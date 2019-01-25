5178d01015a21a6f518176dc8c475d5cf2321305
esarner <e.sarner@gmail.com>
2019-01-26 00:28:44 +0100

    stuff

---
+++ Assets/Pickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private SpriteRenderer _sprite;
+  
+    void Start()
+    {
+        _sprite = GetComponent<SpriteRenderer>();
+    }
+    
+    public void SetFocus(bool focused = true)
+    {
+        _sprite.color = focused ? Color.green : Color.white;
+    }
+}
