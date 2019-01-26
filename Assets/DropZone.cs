c6202d3988a730401354c00f9a15c57ce086ac0a
esarner <e.sarner@gmail.com>
2019-01-26 13:36:49 +0100

    drop zone + updated chips prefabs

---
+++ Assets/DropZone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropZone : MonoBehaviour
+{
+    private readonly List<Pickup> _objectsInDropZone = new List<Pickup>();
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var pickup = other.GetComponent<Pickup>();
+
+        if (pickup != null)
+        {
+            _objectsInDropZone.Add(pickup);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        var pickup = other.GetComponent<Pickup>();
+
+        if (pickup != null && _objectsInDropZone.Contains(pickup))
+        {
+            _objectsInDropZone.Remove(pickup);
+        }
+    }
+}
